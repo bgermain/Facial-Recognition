05d20c39ee886f579c9bf9bd0e1100662a1fb801
Brad Germain <brad@fid3.com>
2012-11-06 13:03:42 -0500

    Created Main Class

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+/**
+ * Facial Recognition
+ * @author Brad Germain and Jeff Tran
+ *
+ */
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 0;
+		System.out.println("HELLO");
+
+	}
+
+}
